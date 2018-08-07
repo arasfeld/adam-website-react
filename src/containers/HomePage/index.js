843c627110e02f5872ea98d495d5963ee7c07908
Adam Rasfeld <arasfeld@gmail.com>
2018-08-07 08:55:41 -0400

    removing feature list from homepage

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -11,11 +11,6 @@ import { FormattedMessage } from 'react-intl';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import Feature from 'components/Feature';
-import LastFmLogo from './images/lastfm.png';
-import MailgunLogo from './images/mailgun.png';
-import ReactLogo from './images/react.png';
-import SteamLogo from './images/steam.png';
 import messages from './messages';
 
 class HomePage extends React.Component {
@@ -42,43 +37,8 @@ class HomePage extends React.Component {
               <FormattedMessage {...messages.header} />
             </Typography>
             <Typography variant="body1" gutterBottom>
-              My name is Adam Rasfeld and this is my personal website. As I dont
-              really have anything particularly interesting to put out into the
-              world, this is more or less a pet project that I started to help
-              myself learn React and other technologies that I am interested in.
+              <FormattedMessage {...messages.about} />
             </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Feature
-              title={<FormattedMessage {...messages.reactTitle} />}
-              body={<FormattedMessage {...messages.reactBody} />}
-              image={ReactLogo}
-              imagePosition="left"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Feature
-              title={<FormattedMessage {...messages.lastFmTitle} />}
-              body={<FormattedMessage {...messages.lastFmBody} />}
-              image={LastFmLogo}
-              imagePosition="right"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Feature
-              title={<FormattedMessage {...messages.steamTitle} />}
-              body={<FormattedMessage {...messages.steamBody} />}
-              image={SteamLogo}
-              imagePosition="left"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Feature
-              title={<FormattedMessage {...messages.mailgunTitle} />}
-              body={<FormattedMessage {...messages.mailgunBody} />}
-              image={MailgunLogo}
-              imagePosition="right"
-            />
           </Grid>
         </Grid>
       </article>
--- src/containers/HomePage/messages.js
+++ src/containers/HomePage/messages.js
@@ -12,40 +12,11 @@ export default defineMessages({
     id: `${scope}.header`,
     defaultMessage: 'Home',
   },
-  lastFmTitle: {
-    id: `${scope}.lastfm.title`,
-    defaultMessage: 'Last.fm API',
-  },
-  lastFmBody: {
-    id: `${scope}.lastfm.body`,
-    defaultMessage:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  },
-  mailgunTitle: {
-    id: `${scope}.mailgun.title`,
-    defaultMessage: 'Mailgun API',
-  },
-  mailgunBody: {
-    id: `${scope}.mailgun.body`,
-    defaultMessage:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  },
-  reactTitle: {
-    id: `${scope}.react.title`,
-    defaultMessage: 'React',
-  },
-  reactBody: {
-    id: `${scope}.react.body`,
-    defaultMessage:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  },
-  steamTitle: {
-    id: `${scope}.steam.title`,
-    defaultMessage: 'Steam API',
-  },
-  steamBody: {
-    id: `${scope}.steam.body`,
-    defaultMessage:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  about: {
+    id: `${scope}.about`,
+    defaultMessage: `My name is Adam Rasfeld and this is my personal website. As I don't
+      really have anything particularly interesting to put out into the
+      world, this is more or less a pet project that I started to help
+      myself learn React and other technologies that I am interested in.`,
   },
 });
