2d341790959c61537b4f8e30a61f0ab596ce1a73
Adam Rasfeld <arasfeld@gmail.com>
2025-09-06 14:19:16 -0400

    add custom logo & favicon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { ThemeToggle } from './theme-toggle';
@@ -8,6 +9,13 @@ export function Header() {
       <div className="max-w-6xl mx-auto flex h-14 items-center justify-between px-6">
         <div className="flex items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
+            <Image
+              src="/logo.svg"
+              alt="Adam Rasfeld Logo"
+              width={32}
+              height={32}
+              className="h-16 w-16"
+            />
             <span className="hidden font-bold sm:inline-block">
               Adam Rasfeld
             </span>
